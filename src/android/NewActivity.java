ab268a7bda409bdd2a889b50e8853e4c269da9ae
Your Name <dharmendra@rytangle.com>
2017-10-21 13:27:38 +0530

    config xml modified10

--- src/android/NewActivity.java
+++ src/android/NewActivity.java
@@ -2,11 +2,12 @@ package com.example.sample.plugin;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
+
 import android.content.Context;
 import android.widget.Toast;
 import android.view.View;
 import android.view.*;
+import android.widget.EditText;
 
 public class NewActivity extends Activity {
 
@@ -17,7 +18,25 @@ public class NewActivity extends Activity {
         setContentView(getApplication().getResources().getIdentifier("activity_new", "layout", package_name));
     }
     public void start(View view) {
-       Log.i("log","close btn clicked");
+       
+                Context context = getApplicationContext();
+                CharSequence text = "Hello toast!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+                  EditText username = (EditText)findViewById(R.id.chat_text);
+
+                Context context = getApplicationContext();
+                CharSequence text = String.valueOf(username);
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+
+   }
+   public void closeme(View view) {
+       
                 Context context = getApplicationContext();
                 CharSequence text = "Hello toast!";
                 int duration = Toast.LENGTH_SHORT;
