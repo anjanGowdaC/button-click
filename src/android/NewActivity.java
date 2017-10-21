0d5b299defa52630b2aa7194649149161ef733b9
Your Name <dharmendra@rytangle.com>
2017-10-21 14:17:29 +0530

    config xml modified17

--- src/android/NewActivity.java
+++ src/android/NewActivity.java
@@ -26,7 +26,7 @@ public class NewActivity extends Activity {
 
                 // Toast toast = Toast.makeText(context, text, duration);
                 // toast.show();
-                  EditText username = (EditText)findViewById(android.R.id.editText1);
+                  EditText username = (EditText)findViewById(getApplication().getResources().getIdentifier("editText1", "id", package_name));
 
                 // Context context = getApplicationContext();
                 CharSequence txt = String.valueOf(username);
