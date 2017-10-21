a66bd6c3e270247ef69d150949a673f8ba4a2e2d
Your Name <dharmendra@rytangle.com>
2017-10-21 11:59:13 +0530

    config xml modified8

--- src/android/NewActivity.java
+++ src/android/NewActivity.java
@@ -2,6 +2,9 @@ package com.example.sample.plugin;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.content.Context;
+import android.widget.Toast;
 
 public class NewActivity extends Activity {
 
@@ -11,4 +14,14 @@ public class NewActivity extends Activity {
         String package_name = getApplication().getPackageName();
         setContentView(getApplication().getResources().getIdentifier("activity_new", "layout", package_name));
     }
+    public void start(View view) {
+       Log.i("log","close btn clicked");
+                Context context = getApplicationContext();
+                CharSequence text = "Hello toast!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+
+   }
 }
